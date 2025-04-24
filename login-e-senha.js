a6c55178ca604c99d9c369cccda32c29fced1cc6
Kauan Dias Lopes <lopeskauandias@gmail.com>
2025-04-24 14:44:53 -0300

    Update login-e-senha.js
    Adição de um looping no cadastro quando não possível o cadastro e uma função para tornar a senha mais forte pois anteriormente tudo poderia ser uma senha

--- login-e-senha.js
+++ login-e-senha.js
@@ -27,50 +27,55 @@ function menu(){
         }
 
     });
-}
+} 
 
 function salvarDados() {
     fs.writeFileSync('usuarios.json', JSON.stringify(usuarios, null, 2), 'utf8');
   }
 
-function loginESenha(){
+  function loginESenha(){
     rl.question('\nLogin: \nDigite seu nome de usuário: ', (nome) => {
-        
         rl.question('Digite sua senha: ', (senha) => {
-        const encontrado = usuarios.find(u => u.login === nome && u.pin === senha);
-        
-        if (encontrado){
-            rl.close();
-            console.log("\nLogin Confirmado!\nSeja Bem Vindo!")
-        }
-        else{
-    
-            console.log("\nLogin Negado!")
-            menu();
-        }
-        
+            const encontrado = usuarios.find(u => u.login === nome && u.pin === senha);
+
+            if (encontrado){
+                console.log("\nLogin Confirmado!\nSeja Bem Vindo!");
+                rl.close();
+            } else {
+                console.log("\nLogin Negado!");
+                loginESenha(); // tenta de novo
+            }
         });
     });
 }
 
 function cadastro(){
     rl.question('\nCadastro:\nDigite seu login de usuário: ', (nome) => {
-        
         const usuarioExistente = usuarios.find(u => u.login === nome);
             if (usuarioExistente) {
                 console.log('Nome de usuário já existe!');
                 cadastro();
                 return;
             }
-
-        rl.question('Digite sua senha: ', (senha) => {
-            usuarios.push({login: nome, pin: senha});
-            console.log('Cadastro Realizado com Sucesso');
-            salvarDados();
-            loginESenha();
-        });
+        cadastroSenha(nome);
     });
     
+}
+
+function cadastroSenha(nome){
+    rl.question('Digite sua senha: ', (senha) => {
+        if (senha.length > 6){
+            usuarios.push({login: nome, pin: senha});
+            console.log('\nCadastro Realizado com Sucesso!!');
+            salvarDados();
+            loginESenha();
+        }
+        else{ 
+            console.log("\nSenha Inválida, digite a senha novamente!\n")
+            cadastroSenha(nome);
+            return;
+        }
+    });
 }
 
 menu();
